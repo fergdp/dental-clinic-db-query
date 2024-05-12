89771e74c6a0eff9082a5a5084f1aaba6dc7a8bd
fernando <gdp.fernando@gmail.com>
2024-05-12 11:17:25 -0300

    added user and roles query

---
+++ users_roles_permitions.sql
@@ -0,0 +1,6 @@
+-- Use the database
+USE dental_clinic_db;
+
+-- Insert roles into the Roles table
+INSERT INTO Roles (RoleName)
+VALUES ('ROLE_ADMINISTRATOR'), ('ROLE_RECEPTIONIST'), ('ROLE_DENTIST'), ('ROLE_DENTAL_ASSISTANT'), ('ROLE_PATIENT');
